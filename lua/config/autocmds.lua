47444a99563ae8ce7e3fc603142dbf3afa7f89ba
Elliot Spaull <espaull@seek.com.au>
2025-08-07 08:34:55 +1000

    Update packages and add mini.files split config

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,31 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local map_split = function(buf_id, lhs, direction)
+  local rhs = function()
+    -- Make new window and set it as target
+    local cur_target = MiniFiles.get_explorer_state().target_window
+    local new_target = vim.api.nvim_win_call(cur_target, function()
+      vim.cmd(direction .. " split")
+      return vim.api.nvim_get_current_win()
+    end)
+
+    MiniFiles.set_target_window(new_target)
+    MiniFiles.go_in({ close_on_file = true })
+  end
+
+  -- Adding `desc` will result into `show_help` entries
+  local desc = "Split " .. direction
+  vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
+end
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "MiniFilesBufferCreate",
+  callback = function(args)
+    local buf_id = args.data.buf_id
+    -- Tweak keys to your liking
+    map_split(buf_id, "<C-s>", "belowright horizontal")
+    map_split(buf_id, "<C-v>", "belowright vertical")
+  end,
+})
